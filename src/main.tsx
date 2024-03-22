dbb202ee95ef6e4c1b259c550d6cef2432b5f3de
Bryan Joseph <bryanllana14@gmail.com>
2024-03-22 12:45:24 -0500

    Context api added

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "@fontsource-variable/onest";
 import './index.css'
+import { BudgetProvider } from './context/BudgetContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </React.StrictMode>,
 )
