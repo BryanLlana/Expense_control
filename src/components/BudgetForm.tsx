2cc807ded7676d26830531d1025c050666e31eb3
Bryan Joseph <bryanllana14@gmail.com>
2024-03-22 12:22:26 -0500

    Budget validate

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,4 +1,14 @@
+import { useMemo, useState } from "react"
+
 const BudgetForm = () => {
+  const [budget, setBudget] = useState(0)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber)
+  }
+
+  const isValidateBudget = useMemo(() => budget > 0, [budget])
+
   return (
     <form className="space-y-5">
       <div className="flex flex-col space-y-5">
@@ -9,12 +19,15 @@ const BudgetForm = () => {
           type="number"
           className="w-full bg-white border border-gray-200 p-2 rounded-md"
           name="budget"
+          value={budget}
+          onChange={handleChange}
         />
       </div>
       <input
         type="submit"
         value='Guardar Presupuesto'
-        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold"
+        disabled={!isValidateBudget}
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold disabled:opacity-50"
       />
     </form>
   )
